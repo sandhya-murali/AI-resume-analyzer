6c8680bb284bc9e89d6611014768f1135a14ba80
sandhya-murali <sandhyapm2004@gmail.com>
2025-08-10 03:43:54 +0530

    second commit

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,13 +1,20 @@
 import type { Route } from "./+types/home";
-import { Welcome } from "../welcome/welcome";
-
+import Navbar from '../components/Navbar';
 export function meta({}: Route.MetaArgs) {
   return [
-    { title: "New React Router App" },
-    { name: "description", content: "Welcome to React Router!" },
+    { title: "aiRes" },
+    { name: "description", content: "Your Personal Resume Analyzer" },
   ];
 }
 
 export default function Home() {
-  return <Welcome />;
+  return <main className="bg-[url('/images/bg-main.svg')] bg-cover">
+    <Navbar/>
+    <section className="main-section">
+      <div className="page-heading">
+        <h1>Track Your Resume Ratings</h1>
+        <h2>Review your submission and get AI-powered feedback</h2>
+      </div>
+    </section>
+  </main>
 }
